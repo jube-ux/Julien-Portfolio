3c0fde849a49ed62aaad81d69a974ffe878e7a75
jube-ux <132795056+jube-ux@users.noreply.github.com>
2024-06-15 00:23:18 -0300

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,75 @@
+// The below code will increase the font size of the menu options once 
+// the mouse hovers over the menu using JQuery
+
+
+
+
+$("body > header > div > div > ul > li:nth-child(1) > a").on( "mouseover", function(){
+    $("body > header > div > div > ul > li:nth-child(1) > a").css("background-color", "white");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(1) > a").on( "mouseout", function(){
+    $("body > header > div > div > ul > li:nth-child(1) > a").css("background-color", "");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(2) > a").on( "mouseover", function(){
+    $("body > header > div > div > ul > li:nth-child(2) > a").css("background-color", "gray");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(2) > a").on( "mouseout", function(){
+    $("body > header > div > div > ul > li:nth-child(2) > a").css("background-color", "");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(3) > a").on( "mouseover", function(){
+    $("body > header > div > div > ul > li:nth-child(3) > a").css("background-color", "gray");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(3) > a").on( "mouseout", function(){
+    $("body > header > div > div > ul > li:nth-child(3) > a").css("background-color", "");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(4) > a").on( "mouseover", function(){
+    $("body > header > div > div > ul > li:nth-child(4) > a").css("background-color", "gray");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(4) > a").on( "mouseout", function(){
+    $("body > header > div > div > ul > li:nth-child(4) > a").css("background-color", "");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(5) > a").on( "mouseover", function(){
+    $("body > header > div > div > ul > li:nth-child(5) > a").css("background-color", "gray");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(5) > a").on( "mouseout", function(){
+    $("body > header > div > div > ul > li:nth-child(5) > a").css("background-color", "");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(6) > a").on( "mouseover", function(){
+    $("body > header > div > div > ul > li:nth-child(6) > a").css("background-color", "gray");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(6) > a").on( "mouseout", function(){
+    $("body > header > div > div > ul > li:nth-child(6) > a").css("background-color", "");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(7) > a").on( "mouseover", function(){
+    $("body > header > div > div > ul > li:nth-child(7) > a").css("background-color", "gray");
+
+});
+
+$("body > header > div > div > ul > li:nth-child(7) > a").on( "mouseout", function(){
+    $("body > header > div > div > ul > li:nth-child(7) > a").css("background-color", "");
+
+});
